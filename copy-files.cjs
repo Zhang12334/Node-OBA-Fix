7411bf37e0858d943a9750bf0d3cc6f0ae7cc1ea
Zhang12334 <3143448821@qq.com>
2025-03-28 11:55:21 +0800

    修复构建 & Release v1.5.5

---
+++ copy-files.cjs
@@ -0,0 +1,9 @@
+const ncp = require('ncp').ncp;
+const path = require('path');
+
+ncp(path.join(__dirname, 'src/dashboard'), path.join(__dirname, 'dist/dashboard'), function (err) {
+  if (err) {
+    return console.error(err);
+  }
+  console.log('构建完成');
+});
